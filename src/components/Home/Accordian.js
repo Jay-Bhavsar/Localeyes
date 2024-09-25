7e8e5c3aa4183ba05fb6c70e54d03424f050d3ba
Jay Bhavsar <87071878+Jay-Bhavsar@users.noreply.github.com>
2024-09-25 14:29:30 +0530

    final faq styling

--- src/components/Home/Accordian.js
+++ src/components/Home/Accordian.js
@@ -7,12 +7,12 @@ export default function DefaultAccordion() {
     <Accordion className="w-[50%]">
       <Accordion.Panel>
         <Accordion.Title>
-          <p>
+          <p className="font-bold">
             1. What is Ayurveda Manuscript Registry of India (AMRRI)?
           </p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             In the context of the Ayurveda Manuscripts Research Registry of
             India, a manuscript registry is a database or collection of
             information related to manuscripts. It refers to a registry
@@ -23,29 +23,31 @@ export default function DefaultAccordion() {
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>
+          <p className="font-bold">
             2. Does the dataset used to register manuscript research vary
             depending on the type of manuscript study?
           </p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             Currently, the research registration dataset is the same for all
             types of manuscript studies.
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>
+          <p className="font-bold">
             3. What documents, if any, are required to be submitted for study
             registration?
           </p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             Submission/upload of permission letter/approval letter from the
             institution or manuscript repository centre (if any) is required.
             In addition, email verification/confirmation of participation in
@@ -54,12 +56,13 @@ export default function DefaultAccordion() {
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>4. Where do these documents have to be submitted?</p>
+          <p className="font-bold">4. Where do these documents have to be submitted?</p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             As AMRRI is a paperless system, all submissions are online. These
             documents are to be uploaded into the system while filling the
             AMRRI application form. Verification is by email wherein the
@@ -68,22 +71,24 @@ export default function DefaultAccordion() {
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>5. Are there any fees for registering research in AMRRI?</p>
+          <p className="font-bold">5. Are there any fees for registering research in AMRRI?</p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             No, there are no fees for registering research.
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>6. Who is responsible for registering the project/trial?</p>
+          <p className="font-bold">6. Who is responsible for registering the project/trial?</p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             A reputable registrant, researcher, or user is authorized to
             register the manuscript registry in AMRRI. The "registrant" for a
             study is either the principal investigator (PI) or the primary
@@ -91,49 +96,55 @@ export default function DefaultAccordion() {
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>7. Why should trials be registered on the MSS R website?</p>
+          <p className="font-bold">7. Why should trials be registered on the MSS R website?</p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             In the future, the AYUSH ministry will make registration of
             Ayurveda manuscript-related research mandatory to avoid repetition
             and to collect metadata related to the same.
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>8. What types of studies are registered in the MSS registry?</p>
+          <p className="font-bold">8. What types of studies are registered in the MSS registry?</p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             All kinds of studies related to Ayurveda manuscripts.
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>9. What are the documents required to be submitted for registration?</p>
+          <p className="font-bold">
+            9. What are the documents required to be submitted for registration?
+          </p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             Consent form/approval form wherever applicable is mandatory. In
             addition, submission of approval from the institution or manuscript
             repository centre (MRC if any).
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>
+          <p className="font-bold">
             10. Where can I get a certificate confirming that my Ayurveda
             manuscript research has been registered?
           </p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             No certificate is issued upon registration. The AMRRI number may be
             quoted wherever required as the registration will be viewable and
             searchable in the public domain (www.amrri.in). Further, a PDF copy
@@ -143,12 +154,13 @@ export default function DefaultAccordion() {
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>11. Where do these documents have to be submitted?</p>
+          <p className="font-bold">11. Where do these documents have to be submitted?</p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             AMRRI is a paperless system. All submissions must be made online,
             and all documents (soft copies) must be uploaded to the database
             while completing the AMRRI form. Verification is by email wherein
@@ -157,36 +169,37 @@ export default function DefaultAccordion() {
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>12. Are there any fees for registering in AMRRI?</p>
+          <p className="font-bold">12. Are there any fees for registering in AMRRI?</p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">No.</p>
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">No.</p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>
-            13. What if my questions or concerns are not answered here?
-          </p>
+          <p className="font-bold">13. What if my questions or concerns are not answered here?</p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             If you have any additional questions or concerns, send an email to
             www.amrri.in with the subject "AMRRI number" in the body.
           </p>
         </Accordion.Content>
       </Accordion.Panel>
+
       <Accordion.Panel>
         <Accordion.Title>
-          <p>
+          <p className="font-bold">
             14. Can I make changes in the Ayurveda manuscript study details
             after it is registered?
           </p>
         </Accordion.Title>
         <Accordion.Content>
-          <p className="mb-2 text-gray-700 dark:text-gray-400">
+          <p className="mb-2 text-gray-700 dark:text-gray-400 text-left">
             To make changes in a registered Ayurveda manuscript study, please
             follow the SOP (which we will update soon on the website).
           </p>
